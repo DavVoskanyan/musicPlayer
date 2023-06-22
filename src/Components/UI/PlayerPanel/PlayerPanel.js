8041fc4173c9847e13c6c67ed5df8bf77521f4ae
David Voskanyan <voskanyandavid001001@gmail.com>
2023-06-22 16:29:03 +0400

    player progress bar

--- src/Components/UI/PlayerPanel/PlayerPanel.js
+++ src/Components/UI/PlayerPanel/PlayerPanel.js
@@ -2,15 +2,24 @@ import { useRef, useState } from 'react';
 
 import styleClasses from './PlayerPanel.module.css';
 
+import PlayerProgressBar from '../PlayerProgressBar/PlayerProgressBar';
 import PlayerButton from "../PlayerButton/PlayerButton";
 import InPlayerSongInfo from "../InPlayerSongInfo/InPlayerSongInfo";
 
 import song from '../../../Songs/testSong.mp3';
 
-const PlayerPanel = () => {
+const PlayerPanel = (props) => {
     const [ isPlaying, changePlayButtonState ] = useState(false);
 
     const audioElement = useRef(null);
+
+    const timeUpdateHandler = () => {
+        const progressPercent = audioElement.current.currentTime / audioElement.current.duration * 100;
+        props.changeProgress(progressPercent);
+    }
+    const progressBarClickHandler = (percent) => {
+        audioElement.current.currentTime = audioElement.current.duration * percent / 100;
+    }
 
     const previousSong = () => console.log('prev');
     const playSong = () => {
@@ -25,8 +34,18 @@ const PlayerPanel = () => {
 
     return (
         <div className={ styleClasses.playerPanel }>
+            <audio src={ song }
+                   ref={ audioElement }
+                   onTimeUpdate={ timeUpdateHandler }/>
+
             <InPlayerSongInfo songName="The Story of Alisher" artistName="Oxxxymiron" featsArray={["Porchy", "Anacondaz"]}/>
             <div className={styleClasses.buttonsPanel}>
+
+                <PlayerProgressBar progressValue={ props.progressValue }
+                                   songDuration={ audioElement.current.duration }
+                                   changeProgress={ props.changeProgress }
+                                   timeUpdateHandler={ timeUpdateHandler }
+                                   progressBarClickHandler={ progressBarClickHandler }/>
 
                 <PlayerButton
                     buttonType="previous"
@@ -41,7 +60,6 @@ const PlayerPanel = () => {
                     buttonClickHandler={ nextSong } />
 
             </div>
-            <audio src={song} ref={audioElement}></audio>
         </div>
     )
 }
---
+++ src/Components/UI/PlayerProgressBar/PlayerProgressBar.js
@@ -0,0 +1,43 @@
+import styleClasses from './PlayerProgressBar.module.css';
+
+const PlayerProgressBar = (props) => {
+
+    const progressTime = props.songDuration * props.progressValue / 100;
+
+    const durationMinutes = Math.floor(props.songDuration / 60);
+    const durationSeconds = Math.floor(props.songDuration % 60) < 10
+        ? `0${Math.floor(props.songDuration % 60)}`
+        : Math.floor(props.songDuration % 60);
+    const durationParser = `${ durationMinutes }:${ durationSeconds }`;
+
+
+    const currentTimeMinutes = Math.floor(progressTime / 60);
+    const currentTimeSeconds = Math.floor(progressTime % 60) < 10
+        ? `0${Math.floor(progressTime % 60)}`
+        : Math.floor(progressTime % 60);
+    const currentTime = `${ currentTimeMinutes }:${ currentTimeSeconds }`;
+
+    const progressBarTouchHandler = (eventObject) => {
+        const positionX = eventObject.clientX;
+        const screenWidth = window.screen.width;
+
+        const progressBarWidth =  window.screen.width * 70 / 100;
+        const progressWidth = positionX - screenWidth * 15 / 100;
+
+        const percent = progressWidth / progressBarWidth * 100;
+
+        props.progressBarClickHandler(percent);
+    }
+
+    return (
+        <div className={ styleClasses.progressBarContainer } onClick={ progressBarTouchHandler }>
+            <span className={ styleClasses.currentTime }>{ currentTime }</span>
+            <div className={ styleClasses.progressBar }>
+                <div className={ styleClasses.progressBarActivePart } style={ { 'width': `${props.progressValue}%` } } />
+            </div>
+            <span className={ styleClasses.totalTime }>{ durationParser }</span>
+        </div>
+    )
+}
+
+export default PlayerProgressBar;
